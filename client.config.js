fe6497f78f73cb46af9113247613b8c8a999273f
IceHax <vecchiotom1@gmail.com>
2019-02-02 15:16:36 +0100

    initial commit

---
+++ client.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/index.js',
+  target: 'node',
+  mode: 'production',
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    __dirname: true,
+  },
+  context: path.resolve(__dirname, 'src'),
+  optimization: {
+    minimize: true,
+  },
+};
---
+++ server.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/server/index.js',
+  target: 'node',
+  mode: 'production',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    __dirname: true,
+  },
+  context: path.resolve(__dirname, 'src'),
+  optimization: {
+    minimize: true,
+  },
+};
